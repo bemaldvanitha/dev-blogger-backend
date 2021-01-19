10cdd790c06506795eff1a00e00d89e8a3298a82
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-01-19 14:06:28 +0530

    connect to database

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 
+const connectDB = require('./config/db');
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// connect database
+connectDB();
 
 app.get('/',(req,res) => {
     return res.send('api running');
