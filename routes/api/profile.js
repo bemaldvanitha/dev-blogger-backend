7d64188a6a21616071b66ef9aa3f2ecc3c16cd8a
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-01-22 14:13:15 +0530

    get current profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,30 @@
 const express = require('express');
+
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
 const router = express.Router();
 
-// @route GET api/profile
-// @desc test route
-// @access Public
+// @route GET api/profile/me
+// @desc Get current user profile
+// @access Private
 
-router.get('/',(req,res) => {
-    return res.send('Profile route');
+router.get('/me',auth, async (req,res) => {
+    try{
+        const profile = await Profile.findOne({ user: req.user.id })
+            .populate('user',['name','avatar']);
+
+        if(!profile){
+            return res.status(400).json({ msg: 'There is no profile for this user' });
+        }
+
+        return res.json(profile);
+
+    }catch (err){
+        console.error(err.message);
+        return res.status(500).send('Server error');
+    }
 });
 
 module.exports = router;
