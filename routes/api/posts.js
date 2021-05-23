4a4cae7fba7f22c62c49c03acff0865a07cd9d9e
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-05-23 10:36:22 +0530

    add new post function added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,12 +1,45 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 
-// @route GET api/posts
-// @desc test route
-// @access Public
+const Post = require('../../models/Post');
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+const auth = require('../../middleware/auth');
 
-router.get('/',(req,res) => {
-    return res.send('Posts route');
+// @route POST api/posts
+// @desc create new post
+// @access Private
+router.post('/',[
+    auth,
+    [
+        check('text', 'Text is required').not().isEmpty()
+    ]
+],async (req,res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    try{
+
+        const user = await User.findById(req.user.id).select('-password');
+
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        });
+
+        const post = await newPost.save();
+        res.json(post);
+
+    }catch (err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
 });
 
 module.exports = router;
